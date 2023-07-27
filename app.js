5af0b1d261b9f9d926d154282f622efec56088e7
nagi <angus.5267@gmail.com>
2023-07-27 10:17:03 +0800

    day two to lern nodeJs

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const fs = require("fs");
+
+fs.writeFile("myFile.txt","今天天氣不錯",(e) => {
+    if (e) throw e ;
+
+    console.log("文件撰寫完畢");
+});
+
+fs.readFile("myFile.txt","utf8",(e,data) => {
+    if(e) throw e ;
+    console.log(data);
+});
+
+
